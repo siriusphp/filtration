625c9901d653992b7670b1300d7266f8c5fb3ae3
Adrian Miu <adrian@adrianmiu.ro>
2013-12-21 18:03:38 +0200

    Implemented the FiltratableTrait

--- src/Filtration/FiltratableTrait.php
+++ src/Filtration/FiltratableTrait.php
@@ -7,15 +7,12 @@ use Sirius\Filtration\Filtrator;
 trait FiltratableTrait {
     protected $filtrator;
 
-    function setValidator($filtrator) {
-        if (!$filtrator instanceof Filtrator) {
-            throw new \InvalidArgumentException('The $filtrator argument is not a proper Filtrator object');
-        }
+    function setFiltrator(Filtrator $filtrator) {
         $this->filtrator = $filtrator;
         return $this;
     }
 
-    function getValidator($filtrator) {
+    function getFiltrator($filtrator) {
         if (!$this->filtrator) {
             $this->filtrator = new Filtrator();
         }
---
+++ tests/FiltrationTest/FiltratableTraitTest.php
@@ -0,0 +1,64 @@
+<?php
+namespace Sirius\Filtration;
+
+use Sirius\Filtration\Filtrator;
+
+class FiltratableForm {
+    use \Sirius\Filtration\FiltratableTrait;
+    
+    protected $values = array();
+    
+    function setValues($values) {
+        $values = $this->getFiltrator()->filter($values);
+        $this->values = $values;
+        return $this;
+    }
+    
+    function getValues() {
+        return $this->values;
+    }
+}
+
+class FiltratableTraitTest extends \PHPUnit_Framework_TestCase
+{
+
+    function setUp()
+    {
+        $this->form = new FiltratableForm();
+        $this->form->getFiltrator()->add(array(
+        	'title' => 'stringtrim | nullify | truncate(limit=10)',
+            'description' => 'stringtrim | nullify'
+        ));
+    }
+
+    function testFilter()
+    {
+        $this->form->setValues(array(
+        	'title' => 'Title is very long',
+            'description' => '    '
+        ));
+        $this->assertEquals(array(
+        	'title' => 'Title is v...',
+            'description' => null
+        ), $this->form->getValues());
+    }
+    
+    function testExceptionOnInvalidFiltrator() {
+        $this->setExpectedException('\Exception');
+        $this->form->setFiltrator(new \stdClass());
+    }
+    
+    function testSettingNewFiltrator() {
+        $this->form->setFiltrator(new Filtrator(array(
+            'title' => 'stringtrim | nullify | truncate(limit=10)',
+        )));
+        $this->form->setValues(array(
+        	'title' => 'Title is very long',
+            'description' => '    '
+        ));
+        $this->assertEquals(array(
+        	'title' => 'Title is v...',
+            'description' => '    '
+        ), $this->form->getValues());
+    }
+}
